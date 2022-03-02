1f08f4c4ee798d63e4b1f87e6e6a6bf2f34fa10a
gmh5225 <2315157@qq.com>
2022-03-02 12:28:01 +0800

    Add __declspec(naked)

--- LLVMIntrinsicRewrite/llvm_Intrinsic_readcr3.c
+++ LLVMIntrinsicRewrite/llvm_Intrinsic_readcr3.c
@@ -1,13 +1,15 @@
 
 
-__declspec(noinline) unsigned __int64 __readcr3(void) {
+__declspec(naked) unsigned __int64 __readcr3(void) {
 #ifdef _WIN64
   _asm {
 		mov rax, cr3
+		ret
   }
 #else
   _asm {
 		mov eax, cr3
+		ret
   }
 #endif // _WIN64
 }
