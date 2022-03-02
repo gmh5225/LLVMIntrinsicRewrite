d2792475e350fb38c2dc5a063b8abe87ca6f8d70
gmh5225 <2315157@qq.com>
2022-03-02 18:46:18 +0800

    Update __readmsr

---
+++ LLVMIntrinsicRewrite/llvm_Intrinsic_readmsr.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#ifndef _WIN64
+__declspec(naked)
+#endif
+    __int64 __readmsr(int reg) {
+#ifdef _WIN64
+  _asm {
+	    xor rax,rax
+		rdmsr
+		shl rdx,32
+		or rax, rdx
+  }
+#else
+  _asm {
+		xor eax, eax
+		xor edx, edx
+		mov ecx, dword ptr[esp + 4]
+		rdmsr
+		ret 4
+  }
+#endif // _WIN64
+}
