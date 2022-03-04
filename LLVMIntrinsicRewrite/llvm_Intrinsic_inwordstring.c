e1fb2f53234248ad7d3e7b003e61843edb949025
gmh5225 <2315157@qq.com>
2022-03-04 15:47:37 +0800

    Update __indwordstring

---
+++ LLVMIntrinsicRewrite/llvm_Intrinsic_inwordstring.c
@@ -0,0 +1,19 @@
+#include <Windows.h>
+
+// https://github.com/MicrosoftDocs/cpp-docs/blob/main/docs/intrinsics/inwordstring.md
+void __inwordstring(unsigned short Port, unsigned short *Buffer,
+                    unsigned long Count) {
+  for (unsigned long i = 0; i < Count; ++i) {
+    unsigned short *pOut = (unsigned short *)(Buffer + i);
+    _asm {
+        mov dx, Port
+#ifdef _WIN64
+        mov rdi, pOut
+#else
+        mov edi, pOut
+#endif
+        insw
+
+    }
+  }
+}
