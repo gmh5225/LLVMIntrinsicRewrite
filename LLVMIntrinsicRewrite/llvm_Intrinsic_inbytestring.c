81cf6f8779e44049ae03b8f5f50b6468707b992a
gmh5225 <2315157@qq.com>
2022-03-04 14:44:35 +0800

    Update __inbytestring

---
+++ LLVMIntrinsicRewrite/llvm_Intrinsic_inbytestring.c
@@ -0,0 +1,19 @@
+#include <Windows.h>
+
+// https://github.com/MicrosoftDocs/cpp-docs/blob/main/docs/intrinsics/inbytestring.md
+void __inbytestring(unsigned short Port, unsigned char *Buffer,
+                    unsigned long Count) {
+  for (unsigned long i = 0; i < Count; ++i) {
+    unsigned char *pOut = (unsigned char *)(Buffer + i);
+    _asm {
+        mov dx, Port
+#ifdef _WIN64
+        mov rdi, pOut
+#else
+        mov edi, pOut
+#endif
+        insb
+
+    }
+  }
+}
