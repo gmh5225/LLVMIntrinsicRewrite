278b65475dc2095cc83c52f22be73164f1eec5ac
gmh5225 <2315157@qq.com>
2022-03-04 17:02:28 +0800

    Update _xbegin _xend

---
+++ LLVMIntrinsicRewrite/llvm_Intrinsic_xbegin.c
@@ -0,0 +1,13 @@
+#include <Windows.h>
+
+// https://www.intel.com/content/www/us/en/develop/documentation/cpp-compiler-developer-guide-and-reference/top/compiler-reference/intrinsics/intrinsics-for-avx2/intrinsics-for-tsx/intrinsics-for-restrict-transactional-mem-ops/xbegin.html
+unsigned int _xbegin() {
+  unsigned int status;
+  __asm {
+        mov   eax, 0xFFFFFFFF
+        xbegin _txnL1
+      _txnL1 :
+        mov   status, eax
+  }
+  return status;
+}
---
+++ LLVMIntrinsicRewrite/llvm_Intrinsic_xend.c
@@ -0,0 +1,6 @@
+#include <Windows.h>
+
+// https://www.intel.com/content/www/us/en/develop/documentation/cpp-compiler-developer-guide-and-reference/top/compiler-reference/intrinsics/intrinsics-for-avx2/intrinsics-for-tsx/intrinsics-for-restrict-transactional-mem-ops/xend.html
+unsigned int _xend() {
+  __asm { xend }
+}
