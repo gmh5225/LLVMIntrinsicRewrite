a7efac9137dd1250afa1d692a0838e52a572eed9
gmh5225 <2315157@qq.com>
2022-03-08 18:27:01 +0800

    Update llvm_Intrinsic_vminstr.c

--- LLVMIntrinsicRewrite/llvm_Intrinsic_vminstr.c
+++ LLVMIntrinsicRewrite/llvm_Intrinsic_vminstr.c
@@ -45,3 +45,106 @@ unsigned char __vmx_vmread(size_t Field, size_t *FieldValue) {
   }
 }
 #endif
+
+// https://github.com/MicrosoftDocs/cpp-docs/blob/main/docs/intrinsics/vmx-vmlaunch.md
+#ifdef _WIN64
+unsigned char __vmx_vmlaunch(void) {
+  _asm {
+	  mov eax, 0
+	  mov ebx, 0
+	  mov ecx, 0
+	  mov edx, 0xf00
+	  mov edi, 0
+	  mov esi, 0
+	  vmlaunch
+
+	  jc _vmlaunch_err
+	  jz _vmlaunch_err_half
+	  xor eax, eax
+	  ret
+	  
+	_vmlaunch_err:
+	  mov eax, 1
+	  ret
+
+    _vmlaunch_err_half:
+      mov eax, 2
+      ret
+  }
+}
+#endif
+
+// https://github.com/MicrosoftDocs/cpp-docs/blob/main/docs/intrinsics/vmx-off.md
+#ifdef _WIN64
+void __vmx_off() {
+  _asm {
+	  vmxoff
+  }
+}
+#endif
+
+// https://github.com/MicrosoftDocs/cpp-docs/blob/main/docs/intrinsics/vmx-vmclear.md
+#ifdef _WIN64
+unsigned char __vmx_vmclear(unsigned __int64 *VmcsPhysicalAddress) {
+  _asm {
+	  push rdi
+	  vmclear [rsp]
+	  pop rdi
+
+	  jc _vmclear_err
+	  jz _vmclear_err2
+	  xor rax, rax
+	  ret
+
+	_vmclear_err:
+	  mov rax, 1
+	  ret
+
+	_vmclear_err2 :
+	  mov rax, 2
+	  ret
+  }
+}
+#endif
+
+// https://github.com/MicrosoftDocs/cpp-docs/blob/main/docs/intrinsics/vmx-vmptrld.md
+#ifdef _WIN64
+int __vmx_vmptrld(unsigned __int64 *VmcsPhysicalAddress) {
+  _asm {
+	  push rdi
+	  vmptrld [rsp]
+	  pop rdi
+	  jc _vmptrld_err
+	  jz _vmptrld_err2
+
+	  xor rax, rax
+	  ret
+
+	_vmptrld_err:
+	  mov rax, 1
+	  ret
+
+	_vmptrld_err2 :
+	  mov rax, 2
+	  ret
+  }
+}
+#endif
+
+// https://github.com/MicrosoftDocs/cpp-docs/blob/main/docs/intrinsics/vmx-vmptrst.md
+#ifdef _WIN64
+void __vmx_vmptrst(unsigned __int64 *VmcsPhysicalAddress) {
+  _asm {
+		vmptrst [rdi]
+  }
+}
+#endif
+
+// https://github.com/MicrosoftDocs/cpp-docs/blob/main/docs/intrinsics/vmx-vmresume.md
+#ifdef _WIN64
+unsigned char __vmx_vmresume(void) {
+  _asm {
+		vmresume
+  }
+}
+#endif
