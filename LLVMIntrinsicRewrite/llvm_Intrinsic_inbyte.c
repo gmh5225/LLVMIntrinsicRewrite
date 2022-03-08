4f70da60c18d663394838d85864dede3f9c08411
gmh5225 <2315157@qq.com>
2022-03-08 18:49:51 +0800

    Update llvm_Intrinsic_inbyte.c

---
+++ LLVMIntrinsicRewrite/llvm_Intrinsic_inbyte.c
@@ -0,0 +1,12 @@
+#include <Windows.h>
+
+// https://github.com/MicrosoftDocs/cpp-docs/blob/main/docs/intrinsics/inbyte.md
+unsigned char __inbyte(unsigned short Port) {
+  unsigned char ret = 0;
+  _asm {
+	    mov dx, Port
+		in al, dx
+		mov ret, al
+  }
+  return ret;
+}
