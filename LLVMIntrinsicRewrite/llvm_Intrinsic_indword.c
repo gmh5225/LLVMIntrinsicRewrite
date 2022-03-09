741bc95ba27998093b65880d23c003fe7c6ffc47
gmh5225 <2315157@qq.com>
2022-03-09 16:14:39 +0800

    Fix bugs

--- LLVMIntrinsicRewrite/llvm_Intrinsic_indword.c
+++ LLVMIntrinsicRewrite/llvm_Intrinsic_indword.c
@@ -1,7 +1,7 @@
 #include <Windows.h>
 
 // https://github.com/MicrosoftDocs/cpp-docs/blob/main/docs/intrinsics/indword.md
-unsigned long __inword(unsigned short Port) {
+unsigned long __indword(unsigned short Port) {
   unsigned long ret = 0;
   _asm {
 	    mov dx, Port
