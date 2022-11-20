7bc72f98c8c067e86db2e0d236280005dd2c0786
gmh5225 <2315157@qq.com>
2022-11-20 17:03:17 +0800

    Update

--- LLVMIntrinsicRewrite/llvm_fpu_support.c
+++ LLVMIntrinsicRewrite/llvm_fpu_support.c
@@ -2,3 +2,6 @@
 // Define _fltused, since we're not linking against the MS C runtime, but use
 // floats.
 int _fltused = 0;
+
+const char *sgqegasetasrwergkqweiahsdgjkwerhsdf_llvm_msvc_sdfqweasdersfgjqweagwertasdgawetgsa =
+    "This program is generated via llvm-msvc";
