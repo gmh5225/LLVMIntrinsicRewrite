d45bb88b967a43f02a59fea9c23d65d1a991de5b
gmh5225 <2315157@qq.com>
2023-05-29 23:55:21 +0800

    Update llvm_Intrinsic_vminstr.c

--- LLVMIntrinsicRewrite/llvm_Intrinsic_vminstr.c
+++ LLVMIntrinsicRewrite/llvm_Intrinsic_vminstr.c
@@ -2,29 +2,20 @@
 
 // https://github.com/MicrosoftDocs/cpp-docs/blob/main/docs/intrinsics/vmx-vmwrite.md
 #ifdef _WIN64
-unsigned char
-__vmx_vmwrite(size_t Field, size_t FieldValue)
+__declspec(naked) unsigned char __vmx_vmwrite(size_t Field, size_t FieldValue)
 {
     _asm {
 		vmwrite rcx, rdx
 		jc _vmwrite_err1
 		jz _vmwrite_err2
-
-		mov rsp, rbp
-		pop rbx
-		pop rbp
 		xor al, al
 		ret
+		
 	  _vmwrite_err1:
-		mov rsp, rbp
-		pop rbx
-		pop rbp
 		mov al, 1
 		ret
+			
 	  _vmwrite_err2:
-		mov rsp, rbp
-		pop rbx
-		pop rbp
 		mov al, 2
 		ret
     }
@@ -33,8 +24,7 @@ __vmx_vmwrite(size_t Field, size_t FieldValue)
 
 // https://github.com/MicrosoftDocs/cpp-docs/blob/main/docs/intrinsics/vmx-vmread.md
 #ifdef _WIN64
-unsigned char
-__vmx_vmread(size_t Field, size_t *FieldValue)
+__declspec(naked) unsigned char __vmx_vmread(size_t Field, size_t *FieldValue)
 {
     _asm {
 
@@ -42,24 +32,14 @@ __vmx_vmread(size_t Field, size_t *FieldValue)
 	  vmread [rdx], rcx
 	  jc _vmread_err1
 	  jz _vmread_err2
-
-	  mov rsp, rbp
-      pop rbx
-	  pop rbp
 	  xor al, al
 	  ret
 
-	_vmread_err1:
-	  mov rsp, rbp
-      pop rbx
-	  pop rbp
+	  _vmread_err1:
 	  mov al, 1
 	  ret
-
-    _vmread_err2:
-	  mov rsp, rbp
-      pop rbx
-	  pop rbp
+		
+      _vmread_err2:
 	  mov al, 2
 	  ret
     }
@@ -68,13 +48,9 @@ __vmx_vmread(size_t Field, size_t *FieldValue)
 
 // https://github.com/MicrosoftDocs/cpp-docs/blob/main/docs/intrinsics/vmx-vmlaunch.md
 #ifdef _WIN64
-unsigned char
-__vmx_vmlaunch(void)
+__declspec(naked) unsigned char __vmx_vmlaunch(void)
 {
     _asm {
-	  mov rsp, rbp
-	  pop rbx
-	  pop rbp
 	  vmlaunch
 	  jc _vmlaunch_err ;if vmlaunch succeed will never be here
 	  jz _vmlaunch_err_half
@@ -95,52 +71,32 @@ __vmx_vmlaunch(void)
 
 // https://github.com/MicrosoftDocs/cpp-docs/blob/main/docs/intrinsics/vmx-off.md
 #ifdef _WIN64
-void
-__vmx_off()
+__declspec(naked) void __vmx_off()
 {
     _asm {
 	  vmxoff
+	  ret
     }
 }
 #endif
 
 // https://github.com/MicrosoftDocs/cpp-docs/blob/main/docs/intrinsics/vmx-vmclear.md
 #ifdef _WIN64
-unsigned char
-__vmx_vmclear(unsigned __int64 *VmcsPhysicalAddress)
+__declspec(naked) unsigned char __vmx_vmclear(unsigned __int64 *VmcsPhysicalAddress)
 {
     _asm {
-	  push rdi
-	  mov rdi, VmcsPhysicalAddress
-	  vmclear [rdi]
-	  pop rdi
+	  vmclear [rcx]
 
 	  jc _vmclear_err
 	  jz _vmclear_err2
-	  
-	  mov     rsp, rbp
-	  pop rbx
-	  pop rdi
-	  pop rbp
-
 	  xor al, al
 	  ret
 
 	_vmclear_err:
-	  mov     rsp, rbp
-	  pop rbx
-	  pop rdi
-	  pop rbp
-
 	  mov al, 1
 	  ret
 
     _vmclear_err2:
-	  mov     rsp, rbp
-	  pop rbx
-	  pop rdi
-	  pop rbp
-
 	  mov al, 2
 	  ret
     }
@@ -149,41 +105,22 @@ __vmx_vmclear(unsigned __int64 *VmcsPhysicalAddress)
 
 // https://github.com/MicrosoftDocs/cpp-docs/blob/main/docs/intrinsics/vmx-vmptrld.md
 #ifdef _WIN64
-int
-__vmx_vmptrld(unsigned __int64 *VmcsPhysicalAddress)
+__declspec(naked) int __vmx_vmptrld(unsigned __int64 *VmcsPhysicalAddress)
 {
     _asm {
-	  push rdi
-	  mov rdi, VmcsPhysicalAddress
-
-	  vmptrld [rdi]
-	  pop rdi
+	  vmptrld [rcx]
+	
 	  jc _vmptrld_err
 	  jz _vmptrld_err2
-
-	  mov     rsp, rbp
-	  pop rbx
-	  pop rdi
-	  pop rbp
 
 	  xor eax, eax
 	  ret
 
 	_vmptrld_err:
-	  mov     rsp, rbp
-	  pop rbx
-	  pop rdi
-	  pop rbp
-
 	  mov eax, 1
 	  ret
 
     _vmptrld_err2:
-	  mov     rsp, rbp
-	  pop rbx
-	  pop rdi
-	  pop rbp
-
 	  mov eax, 2
 	  ret
     }
@@ -192,27 +129,20 @@ __vmx_vmptrld(unsigned __int64 *VmcsPhysicalAddress)
 
 // https://github.com/MicrosoftDocs/cpp-docs/blob/main/docs/intrinsics/vmx-vmptrst.md
 #ifdef _WIN64
-void
-__vmx_vmptrst(unsigned __int64 *VmcsPhysicalAddress)
+__declspec(naked) void __vmx_vmptrst(unsigned __int64 *VmcsPhysicalAddress)
 {
     _asm {
-	    push rdi
-		mov rdi, VmcsPhysicalAddress
-		vmptrst [rdi]
-		pop rdi
+		vmptrst [rcx]
+		ret
     }
 }
 #endif
 
 // https://github.com/MicrosoftDocs/cpp-docs/blob/main/docs/intrinsics/vmx-vmresume.md
 #ifdef _WIN64
-unsigned char
-__vmx_vmresume(void)
+__declspec(naked) unsigned char __vmx_vmresume(void)
 {
     _asm {
-		mov rsp, rbp
-		pop rbx
-		pop rbp
 		vmresume
 		jc _vmresume_err ;if vmresume succeed will never be here
 	    jz _vmresume_err_half
@@ -233,29 +163,17 @@ __vmx_vmresume(void)
 
 // https://github.com/MicrosoftDocs/cpp-docs/blob/main/docs/intrinsics/vmx-on.md
 #ifdef _WIN64
-unsigned char
-__vmx_on(unsigned __int64 *VmsSupportPhysicalAddress)
+__declspec(naked) unsigned char __vmx_on(unsigned __int64 *VmsSupportPhysicalAddress)
 {
     _asm {
-	  push rdi
-	  mov rdi, VmsSupportPhysicalAddress
-	  vmxon [rdi]
-	  pop rdi
+	  vmxon [rcx]
 
 	  jc _vmxon_err
-	  mov rsp, rbp
-	  pop rbx
-	  pop rdi
-	  pop rbp
 
 	  xor al, al
 	  ret
 
 	_vmxon_err:
-	  mov rsp, rbp
-	  pop rbx
-	  pop rdi
-	  pop rbp
 
 	  mov al, 1
 	  ret
