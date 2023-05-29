edcf1e1fb2e554584f93775da17c3e268dccde0e
gmh5225 <2315157@qq.com>
2023-05-30 00:19:55 +0800

    Update llvm_Intrinsic_inxxx.c

--- LLVMIntrinsicRewrite/llvm_Intrinsic_inword.c
+++ LLVMIntrinsicRewrite/llvm_Intrinsic_inword.c
@@ -4,11 +4,7 @@
 unsigned short
 __inword(unsigned short Port)
 {
-    unsigned short ret = 0;
-    _asm {
-	    mov dx, Port
-		in ax, dx
-		mov ret, ax
-    }
-    return ret;
+    unsigned short word;
+    __asm__ __volatile__("inw %w[Port], %w[word]" : [word] "=a"(word) : [Port] "Nd"(Port));
+    return word;
 }
--- LLVMIntrinsicRewrite/llvm_Intrinsic_inwordstring.c
+++ LLVMIntrinsicRewrite/llvm_Intrinsic_inwordstring.c
@@ -4,18 +4,8 @@
 void
 __inwordstring(unsigned short Port, unsigned short *Buffer, unsigned long Count)
 {
-    for (unsigned long i = 0; i < Count; ++i)
-    {
-        unsigned short *pOut = (unsigned short *)(Buffer + i);
-        _asm {
-        mov dx, Port
-#ifdef _WIN64
-        mov rdi, pOut
-#else
-        mov edi, pOut
-#endif
-        insw
-
-        }
-    }
+    __asm__ __volatile__("rep; insw"
+                         : [Buffer] "=D"(Buffer), [Count] "=c"(Count)
+                         : "d"(Port), "[Buffer]"(Buffer), "[Count]"(Count)
+                         : "memory");
 }
