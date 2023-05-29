72a778a0faf60df44b1344005e45eaf4a308cf4b
gmh5225 <2315157@qq.com>
2023-05-29 22:05:06 +0800

    Upload llvm_Intrinsic_sidtlidt.c

---
+++ LLVMIntrinsicRewrite/llvm_Intrinsic_sidtlidt.c
@@ -0,0 +1,19 @@
+// https://github.com/MicrosoftDocs/cpp-docs/blob/main/docs/intrinsics/sidt.md
+__declspec(naked) void __sidt(void *Destination)
+{
+    _asm
+    {
+		sidt    fword ptr [rcx]
+        ret
+    }
+}
+
+// https://github.com/MicrosoftDocs/cpp-docs/blob/main/docs/intrinsics/lidt.md
+__declspec(naked) void __lidt(void *Source)
+{
+    _asm
+    {
+		lidt    fword ptr [rcx]
+        ret
+    }
+}
