c58362e2b609eb2eaa64082817e82f41c3c56eff
gmh5225 <2315157@qq.com>
2023-05-30 00:33:34 +0800

    Upload llvm_Intrinsic_gsxxx.c

---
+++ LLVMIntrinsicRewrite/llvm_Intrinsic_gsxxx.c
@@ -0,0 +1,65 @@
+void
+__writegsbyte(unsigned long Offset, unsigned char Data)
+{
+    __asm__ __volatile__("movb %b[Data], %%gs:%a[Offset]" : : [Offset] "ir"(Offset), [Data] "ir"(Data) : "memory");
+}
+
+void
+__writegsword(unsigned long Offset, unsigned short Data)
+{
+    __asm__ __volatile__("movw %w[Data], %%gs:%a[Offset]" : : [Offset] "ir"(Offset), [Data] "ir"(Data) : "memory");
+}
+
+void
+__writegsdword(unsigned long Offset, unsigned long Data)
+{
+    __asm__ __volatile__("movl %k[Data], %%gs:%a[Offset]" : : [Offset] "ir"(Offset), [Data] "ir"(Data) : "memory");
+}
+
+void
+__writegsqword(unsigned long Offset, unsigned long long Data)
+{
+    __asm__ __volatile__("movq %q[Data], %%gs:%a[Offset]" : : [Offset] "ir"(Offset), [Data] "ir"(Data) : "memory");
+}
+
+void
+__incgsbyte(unsigned long Offset)
+{
+    __asm__ __volatile__("incb %%gs:%a[Offset]" : : [Offset] "ir"(Offset) : "memory");
+}
+
+void
+__incgsword(unsigned long Offset)
+{
+    __asm__ __volatile__("incw %%gs:%a[Offset]" : : [Offset] "ir"(Offset) : "memory");
+}
+
+void
+__incgsdword(unsigned long Offset)
+{
+    __asm__ __volatile__("incl %%gs:%a[Offset]" : : [Offset] "ir"(Offset) : "memory");
+}
+
+void
+__incgsqword(unsigned long Offset)
+{
+    __asm__ __volatile__("incq %%gs:%a[Offset]" : : [Offset] "ir"(Offset) : "memory");
+}
+
+void
+__addgsbyte(unsigned long Offset, unsigned char Data)
+{
+    __asm__ __volatile__("addb %b[Data], %%gs:%a[Offset]" : : [Offset] "ir"(Offset), [Data] "ir"(Data) : "memory");
+}
+
+void
+__addgsword(unsigned long Offset, unsigned short Data)
+{
+    __asm__ __volatile__("addw %w[Data], %%gs:%a[Offset]" : : [Offset] "ir"(Offset), [Data] "ir"(Data) : "memory");
+}
+
+void
+__addgsdword(unsigned long Offset, unsigned long Data)
+{
+    __asm__ __volatile__("addl %k[Data], %%gs:%a[Offset]" : : [Offset] "ir"(Offset), [Data] "ir"(Data) : "memory");
+}
