d8414834167f0fbbda5fc8cb87fd56e56da8fc51
gmh5225 <2315157@qq.com>
2023-07-13 09:41:46 +0800

    Update

--- LLVMIntrinsicRewrite/llvm_Intrinsic_readmsr.c
+++ LLVMIntrinsicRewrite/llvm_Intrinsic_readmsr.c
@@ -1,25 +1,19 @@
 
 
 // https://github.com/MicrosoftDocs/cpp-docs/blob/main/docs/intrinsics/readmsr.md
-__declspec(naked) __int64 __readmsr(int reg)
-{
 #ifdef _WIN64
-    _asm {
-		push rdx
-	    xor rax, rax
-		rdmsr
-		shl rdx, 32
-		or rax, rdx
-		pop rdx
-		ret
-    }
-#else
-    _asm {
-		xor eax, eax
-		xor edx, edx
-		mov ecx, dword ptr[esp + 4]
-		rdmsr
-		ret 4
-    }
-#endif // _WIN64
+unsigned __int64
+__readmsr(unsigned long __register)
+{
+    // Loads the contents of a 64-bit model specific register (MSR) specified in
+    // the ECX register into registers EDX:EAX. The EDX register is loaded with
+    // the high-order 32 bits of the MSR and the EAX register is loaded with the
+    // low-order 32 bits. If less than 64 bits are implemented in the MSR being
+    // read, the values returned to EDX:EAX in unimplemented bit locations are
+    // undefined.
+    unsigned long __edx;
+    unsigned long __eax;
+    __asm__("rdmsr" : "=d"(__edx), "=a"(__eax) : "c"(__register));
+    return (((unsigned __int64)__edx) << 32) | (unsigned __int64)__eax;
 }
+#endif
