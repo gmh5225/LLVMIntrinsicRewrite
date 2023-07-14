2b6ca1cf8cbac08be32e3e5901e829afec722be4
gmh5225 <2315157@qq.com>
2023-07-14 12:03:47 +0800

    Update

--- LLVMIntrinsicRewrite/llvm_Intrinsic_invlpg.c
+++ LLVMIntrinsicRewrite/llvm_Intrinsic_invlpg.c
@@ -1,20 +1,8 @@
 #include <Windows.h>
 
 // https://github.com/MicrosoftDocs/cpp-docs/blob/main/docs/intrinsics/invlpg.md
-__declspec(naked) void __invlpg(void *Address)
+void
+__invlpg(void *Address)
 {
-#ifdef _WIN64
-    _asm {
-		invlpg [rcx]
-		ret
-    }
-#else
-    _asm {
-	    push eax
-		mov eax, dword ptr[esp + 8]
-	    invlpg [eax]
-		pop eax
-	    ret 4
-    }
-#endif // _WIN64
+    __asm__ __volatile__("invlpg (%[Address])" : : [Address] "b"(Address) : "memory");
 }
